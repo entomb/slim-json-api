4b0f13a4818c612e81970a60e4400913414a30aa
Jonathan Tavares <the.entomb@gmail.com>
2013-05-17 17:49:13 +0100

    git init

---
+++ jsonAPI/JsonApiMiddleware.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * jsonAPI - Slim extention to implement fast JSON API's
+ *
+ * @package Slim
+ * @subpackage Middleware
+ * @author Jonathan Tavares <the.entomb@gmail.com>
+ * @license GNU General Public License, version 3
+ * @filesource
+ *
+ *
+*/
+
+/**
+ * JsonApiMiddleware - Middleware that sets a bunch of static routes for easy bootstrapping of json API's
+ *
+ * @package Slim
+ * @subpackage View
+ * @author Jonathan Tavares <the.entomb@gmail.com>
+ * @license GNU General Public License, version 3
+ * @filesource
+ */
+class JsonApiMiddleware extends \Slim\Middleware {
+
+
+    /**
+     * Sets a buch of static API calls
+     *
+     */
+    function __construct(){
+
+        $app = \Slim\Slim::getInstance();
+
+        // Mirrors the API request
+        $app->get('/return', function() use ($app) {
+            $app->render(200,array(
+                'method'    => $app->request()->getMethod(),
+                'name'      => $app->request()->get('name'),
+                'headers'   => $app->request()->headers(),
+                'params'    => $app->request()->params(),
+            ));
+        });
+
+        // Generic error handler
+        $app->error(function (Exception $e) use ($app) {
+            $app->render(500,array(
+                'error' => TRUE,
+                'msg'   => $e->getMessage(),
+            ));
+        });
+
+        // Not found handler (invalid routes, invalid method types)
+        $app->notFound(function() use ($app) {
+            $app->render(400,array(
+                'error' => TRUE,
+                'msg'   => 'Invalid route',
+            ));
+        });
+
+        // Handle Empty response body
+        $app->hook('slim.after.router', function () use ($app) {
+            if (strlen($app->response()->body()) == 0) {
+                $app->render(500,array(
+                    'error' => TRUE,
+                    'msg'   => 'Empty response',
+                ));
+            }
+        });
+
+    }
+
+    /**
+     * Call default SLIM call()
+     */
+    function call(){
+        return $this->app->call();
+    }
+
+}
---
+++ jsonAPI/JsonApiView.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * jsonAPI - Slim extention to implement fast JSON API's
+ *
+ * @package Slim
+ * @subpackage View
+ * @author Jonathan Tavares <the.entomb@gmail.com>
+ * @license GNU General Public License, version 3
+ * @filesource
+ *
+ *
+*/
+
+
+/**
+ * JsonApiView - view wrapper for json responses (with error code).
+ *
+ * @package Slim
+ * @subpackage View
+ * @author Jonathan Tavares <the.entomb@gmail.com>
+ * @license GNU General Public License, version 3
+ * @filesource
+ */
+class JsonApiView extends \Slim\View {
+
+    public function render($status=200) {
+
+        $app = \Slim\Slim::getInstance();
+
+        //append error bool
+        if(!isset($this->data['error'])){
+            $this->appendData(array('error'=>false));
+        }
+
+        $this->status = (int)$status;
+
+        //append status code
+        if(isset($this->status)){
+            $this->appendData(array('status'=>$this->status));
+        }
+
+        //remove useless flash data
+        unset($this->data['flash']);
+
+        $app->response()->status($this->status);
+        $app->response()->header('Content-Type', 'application/json');
+        $app->response()->body(json_encode($this->data));
+
+        $app->stop();
+    }
+
+}
