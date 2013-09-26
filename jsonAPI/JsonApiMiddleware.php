5ccd887496357785e093bf69d7d92ce4477ff9e3
Jonathan Tavares <the.entomb@gmail.com>
2013-09-26 23:35:50 +0100

    upgrade on the error reporting

--- jsonAPI/JsonApiMiddleware.php
+++ jsonAPI/JsonApiMiddleware.php
@@ -30,9 +30,11 @@ class JsonApiMiddleware extends \Slim\Middleware {
     function __construct(){
 
         $app = \Slim\Slim::getInstance();
+        $app->config('debug', false);
 
         // Mirrors the API request
         $app->get('/return', function() use ($app) {
+
             $app->render(200,array(
                 'method'    => $app->request()->getMethod(),
                 'name'      => $app->request()->get('name'),
@@ -43,15 +45,17 @@ class JsonApiMiddleware extends \Slim\Middleware {
 
         // Generic error handler
         $app->error(function (Exception $e) use ($app) {
+
+
             $app->render(500,array(
-                'error' => TRUE,
-                'msg'   => $e->getMessage(),
+                'error' => true,
+                'msg'   => \JsonApiMiddleware::_errorType($e->getCode()) .": ". $e->getMessage(),
             ));
         });
 
         // Not found handler (invalid routes, invalid method types)
         $app->notFound(function() use ($app) {
-            $app->render(400,array(
+            $app->render(404,array(
                 'error' => TRUE,
                 'msg'   => 'Invalid route',
             ));
@@ -80,6 +84,43 @@ class JsonApiMiddleware extends \Slim\Middleware {
      */
     function call(){
         return $this->app->call();
+    }
+
+    static function _errorType($type=1){
+        switch($type)
+        {
+            default:
+            case E_ERROR: // 1 //
+                return 'ERROR';
+            case E_WARNING: // 2 //
+                return 'WARNING';
+            case E_PARSE: // 4 //
+                return 'PARSE';
+            case E_NOTICE: // 8 //
+                return 'NOTICE';
+            case E_CORE_ERROR: // 16 //
+                return 'CORE_ERROR';
+            case E_CORE_WARNING: // 32 //
+                return 'CORE_WARNING';
+            case E_CORE_ERROR: // 64 //
+                return 'COMPILE_ERROR';
+            case E_CORE_WARNING: // 128 //
+                return 'COMPILE_WARNING';
+            case E_USER_ERROR: // 256 //
+                return 'USER_ERROR';
+            case E_USER_WARNING: // 512 //
+                return 'USER_WARNING';
+            case E_USER_NOTICE: // 1024 //
+                return 'USER_NOTICE';
+            case E_STRICT: // 2048 //
+                return 'STRICT';
+            case E_RECOVERABLE_ERROR: // 4096 //
+                return 'RECOVERABLE_ERROR';
+            case E_DEPRECATED: // 8192 //
+                return 'DEPRECATED';
+            case E_USER_DEPRECATED: // 16384 //
+                return 'USER_DEPRECATED';
+        }
     }
 
 }
